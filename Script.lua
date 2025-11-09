4c25d7b4a43972d0231096d1084a1c648125e289
okk76 <kipup345678@gmail.com>
2025-11-09 21:43:59 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -233,3 +233,63 @@ local Button = Tab:CreateButton({
 
 end,
 })
+
+local Toggle = Tab:CreateButton({
+   Name = "Shuffle Line Sounds",
+   Callback = function(Value)
+	local newgrab = game.ReplicatedFirst.GrabParts.GrabPart.AttachSound
+	local oldgrab = game.ReplicatedFirst.GrabParts.GrabPart.AttachSound1
+	local newhold = game.ReplicatedFirst.GrabParts.GrabPart.BeamSound
+	local oldhold = game.ReplicatedFirst.GrabParts.GrabPart.BeamSound1
+	newgrab.Name = "AttachSound1"
+    oldgrab.Name = "AttachSound"
+	newhold.Name = "BeamSound1"
+    oldhold.Name = "BeamSound"
+	print("Done!")
+
+		Rayfield:Notify({
+   Title = "Shuffle Line Sounds",
+   Content = "Done",
+   Duration = 3,
+   Image = 4483362458,
+})
+
+   end,
+})
+
+local Tab = Window:CreateTab("Other Scripts", 4483362458)
+
+local Section = Tab:CreateSection("Settings")
+
+local Button = Tab:CreateButton({
+   Name = "InfiniteYield",
+   Callback = function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/refs/heads/master/source"))()
+   
+	print("Done!")
+
+	Rayfield:Notify({
+   Title = "Infinite Yield",
+   Content = "Done",
+   Duration = 3,
+   Image = 4483362458,
+})
+
+end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Dex",
+   Callback = function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkNetworks/Infinite-Yield/refs/heads/main/dex.lua"))()
+	print("Done!")
+
+	Rayfield:Notify({
+   Title = "Dex",
+   Content = "Done",
+   Duration = 3,
+   Image = 4483362458,
+})
+
+end,
+})
