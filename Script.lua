c20254fd3e7f35201bb1cdf8b1aada94a317d04d
okk76 <kipup345678@gmail.com>
2025-11-30 21:15:52 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -364,7 +364,7 @@ local Toggle = Tab:CreateButton({
 	f.CanQuery = false
 	f.CFrame = t.CFrame * CFrame.Angles(0, math.rad(180), 0)
 	wait(0.03)
-	f.CFrame = f.CFrame + Vector3.new(0, -0.9, 0)
+	f.CFrame = f.CFrame + Vector3.new(0, -0.85, 0)
 	wait(0.03)
 	f.CanTouch = true
 	f.CanQuery = true
