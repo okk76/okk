e5df794e5dc191ac2d06d670e70e9aa32d9aec36
okk76 <kipup345678@gmail.com>
2025-12-01 11:35:24 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -530,7 +530,7 @@ local Toggle = Tab:CreateToggle({
    Name = "Anti Fire",
    CurrentValue = false,
    Callback = function(Value)
-       local T = game.workspace.Map.Hole.PosionBigHole.ExtinguishPart
+       local T = game.workspace.Map.Hole.PoisonBigHole.ExtinguishPart
        local old = T.Position
        
        if Value then
